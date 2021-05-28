64a2a0e52246c60f00e8a7932fe822405ba8b4bc
igor <igor.kramaric@gmail.com>
2021-05-28 17:33:50 +0200

    pyximport

--- daffodil/__init__.py
+++ daffodil/__init__.py
@@ -1,1 +1,11 @@
+import pyximport
+pyximport.install()
+
+from .daffodil_parser import Daffodil
+from .predicate import DictionaryPredicateDelegate
+from .hstore_predicate import HStoreQueryDelegate
+from .pretty_print import PrettyPrintDelegate
+from .key_expectation_delegate import KeyExpectationDelegate
+from .simulation_delegate import SimulationMatchingDelegate
+
 default_app_config = 'daffodil.apps.DaffodilAppConfig'
