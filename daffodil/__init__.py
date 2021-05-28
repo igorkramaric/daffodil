70263c47b1219508223eb9128e2e90a224a06bf0
igor <igor.kramaric@gmail.com>
2021-05-28 09:34:12 +0200

    init.py try

---
+++ daffodil/__init__.py
@@ -0,0 +1,6 @@
+from .daffodil_parser import Daffodil
+from .predicate import DictionaryPredicateDelegate
+from .hstore_predicate import HStoreQueryDelegate
+from .pretty_print import PrettyPrintDelegate
+from .key_expectation_delegate import KeyExpectationDelegate
+from .simulation_delegate import SimulationMatchingDelegate
