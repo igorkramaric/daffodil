4e4418cd3e2a686725eb93e2567321d8ac4c2a15
igor <igor.kramaric@gmail.com>
2021-05-28 16:42:46 +0200

    another try

--- daffodil/__init__.py
+++ daffodil/__init__.py
@@ -1,8 +1,1 @@
-from .daffodil_parser import Daffodil
-from .predicate import DictionaryPredicateDelegate
-from .hstore_predicate import HStoreQueryDelegate
-from .pretty_print import PrettyPrintDelegate
-from .key_expectation_delegate import KeyExpectationDelegate
-from .simulation_delegate import SimulationMatchingDelegate
-
 default_app_config = 'daffodil.apps.DaffodilAppConfig'
